a37c4f5bb6ec4b1f2ea39d059e7ed2983dc14aef
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2025-06-09 20:29:37 +0500

    fix: дефолтная строка для конекта

--- services/auth/core/config.py
+++ services/auth/core/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Settings:
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@postgres:5432/sudoku_db")
 
 
 settings = Settings()
