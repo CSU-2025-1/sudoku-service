88b0c688277973e88c963a3fd2fdfaaee157efc7
DarkCyberon <Xcom.D@yandex.ru>
2025-06-10 16:42:23 +0500

    jwt fix

--- services/auth/jwt_token/jwt_gen.py
+++ services/auth/jwt_token/jwt_gen.py
@@ -1,23 +1,21 @@
-from jwt import JWT, exceptions
-import datetime
+import jwt
+from datetime import datetime
 
 JWT_EXP_DELTA_SECONDS = 3600  # Время жизни токена в секундах (1 час)
 
 
-def generate_jwt(username):
-    jwt = JWT()
-    payload = {
-        'username': username,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=JWT_EXP_DELTA_SECONDS)
-    }
-    token = jwt.encode(payload=payload)
-    return token
+def generate_jwt(username, ttl):
+    current_timestamp = datetime.now()
+
+    data = dict(
+        sub=username,
+        iat=current_timestamp.__str__(),
+        nbf=current_timestamp.__str__(),
+        exp=(current_timestamp+ttl).strftime("%Y-%m-%d %H:%M:%S.%f")
+    )
+
+    return jwt.encode(payload=data, key='JWT_SECRET', algorithm='HS256')
 
 
 def verify_jwt(token):
-    jwt = JWT()
-    try:
-        payload = jwt.decode(token)
-        return payload['username']
-    except exceptions.JWTException:
-        return None
+    pass
